34ecb440891a56658bed8792934f58b0989ad57b
iaroslavklopov <iaroslavklopov@vk.com>
2022-05-06 14:25:31 +0300

    реализован запрет на регистрацию нового пользователя с уже существующим логином

--- AuthLog/Registration.cs
+++ AuthLog/Registration.cs
@@ -18,16 +18,30 @@ namespace AuthLog
         {
             InitializeComponent();
         }
-
+        bool _isRegistered = false;
         private void button1_Click(object sender, EventArgs e)
         {
             using (UserContext db = new UserContext())
             {
                 if (textBoxEmail.Text != "" && textBoxLog.Text != "" && textBoxPass.Text != "" )
                 {
-                    User user = new User(textBoxLog.Text, HashClass.GetHashString(textBoxPass.Text), textBoxEmail.Text, "User");
-                    db.Users.Add(user);
-                    db.SaveChanges();
+                    foreach (User user1 in db.Users)
+                    {
+                        if (user1.Login == textBoxLog.Text)
+                        {
+                           _isRegistered = true;
+                        }
+                    }
+                    if (_isRegistered == false)
+                    {
+                        User user = new User(textBoxLog.Text, HashClass.GetHashString(textBoxPass.Text), textBoxEmail.Text, "User");
+                        db.Users.Add(user);
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        MessageBox.Show("This login had created");
+                    }
                 }
                 else
                 {
